4039e0bd6a76317d47385208a0bb2077b807c340
Nemo <me@captnemo.in>
2024-07-30 16:49:31 +0530

    Enforce datetime.date

--- src/ecourts/entities.py
+++ src/ecourts/entities.py
@@ -34,31 +34,33 @@ class Business:
     case_number1: str
     state_code: str
     disposal_flag: str
-    businessDate: str
+    businessDate: datetime.date
     court_no: str
     srno: str
 
     def __post_init__(self):
-        self.nextdate1 = parse_date(self.nextdate1)
-        self.businessDate = parse_date(self.businessDate)
+        if self.nextdate1 instanceof str:
+            self.nextdate1 = parse_date(self.nextdate1)
+        if self.businessDate instanceof str:
+            self.businessDate = parse_date(self.businessDate)
 
 
 @dataclass
 class HistoryEntry:
     cause_list_type: str
     judge: str
-    business_on_date: Optional[datetime]
-    hearing_date: Optional[datetime]
+    business_on_date: Optional[datetime.date]
+    hearing_date: Optional[datetime.date]
     purpose_of_hearing: str
     business: Optional[Business]
 
 
 @dataclass
 class Objection:
-    scrutiny_date: Optional[datetime]
+    scrutiny_date: Optional[datetime.date]
     objection: str
-    compliance_date: Optional[datetime]
-    receipt_date: Optional[datetime]
+    compliance_date: Optional[datetime.date]
+    receipt_date: Optional[datetime.date]
 
 
 @dataclass
@@ -67,10 +69,10 @@ class Case:
     filing_number: str
 
     registration_number: str
-    registration_date: Optional[datetime]
+    registration_date: Optional[datetime.date]
     cnr_number: str
-    first_hearing_date: Optional[datetime]
-    decision_date: Optional[datetime]
+    first_hearing_date: Optional[datetime.date]
+    decision_date: Optional[datetime.date]
     case_status: str
     nature_of_disposal: str
     coram: Optional[str]
@@ -87,14 +89,13 @@ class Case:
     sub_category: Optional[str]
     objections: Optional[List[Objection]]
     not_before_me: Optional[str]
-    filing_date: Optional[datetime] = None
+    filing_date: Optional[datetime.date] = None
 
     def __post_init__(self):
         if not self.filing_date:
             self.filing_date = None
         if self.not_before_me == "":
             self.not_before_me = None
-
 
 @dataclass
 class Court:
